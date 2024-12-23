f07d3fdfd4c60c32760c5e3fb3e85f64c5c5a9c8
praison <praisonvarghese1@gmail.com>
2024-12-23 13:06:28 +0530

    Logs added

--- src/main/java/com/hospital/tm/TicketManagementApplication.java
+++ src/main/java/com/hospital/tm/TicketManagementApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TicketManagementApplication {
 
 	public static void main(String[] args) {
+		System.out.println("TICKET MANAGEMENT APP");
 		SpringApplication.run(TicketManagementApplication.class, args);
 	}
 
