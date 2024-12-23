2cf2078c339c4b5011cfee24f48dde4d0ab1101f
praison <praisonvarghese1@gmail.com>
2024-12-23 13:07:51 +0530

    Revert "Logs added"
    This reverts commit f07d3fdfd4c60c32760c5e3fb3e85f64c5c5a9c8.

--- src/main/java/com/hospital/tm/TicketManagementApplication.java
+++ src/main/java/com/hospital/tm/TicketManagementApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TicketManagementApplication {
 
 	public static void main(String[] args) {
-		System.out.println("TICKET MANAGEMENT APP");
 		SpringApplication.run(TicketManagementApplication.class, args);
 	}
 
